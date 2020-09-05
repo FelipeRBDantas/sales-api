79615ef6360951727fdf3bcfc79a51604ec0f0e5
Felipe Dantas <feliperbdantas@outlook.com>
2020-09-05 12:49:35 -0300

    Update name database in migration

--- src/main/resources/db/migration/V1__create_categories_table.sql
+++ src/main/resources/db/migration/V1__create_categories_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `category` (
+CREATE TABLE IF NOT EXISTS `categories` (
     `id` bigint(20) NOT NULL AUTO_INCREMENT,
     `name` varchar(45) NOT NULL,
     `description` varchar(100) NOT NULL,
